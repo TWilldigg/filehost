4e0d68a6d309f7e67b2ff5c5adab289c2a24ae66
twilldigg <10twilldigg@gmail.com>
2026-03-23 11:45:04 +0000

    Consolidated files and improved UX

--- index.php
+++ index.php
@@ -1,14 +1,18 @@
 <?php
-// Needs complete rework
 $debug = false;
-        ini_set('display_errors', 1);
-        ini_set('display_startup_errors', 1);
-        error_reporting(E_ALL);
+if($debug){
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+}
+//Variables that will see the html.
+$message = '';
+$url = '';
 
 $uploadDir = __DIR__ . '/uploads/';
 if (!is_dir($uploadDir)) {
 	mkdir($uploadDir, 0755, true);
-};
+}
 
 
 //Establish DB Connection Info
@@ -29,7 +33,7 @@ $options = [
 
 //CHECK if FILES is empty
 //generate and insert new DB Field.
-
+if ($_SERVER['REQUEST_METHOD'] === 'POST'){
 if (!empty($_FILES['files'])) {
 	$tmp = $_FILES['files']['tmp_name'][0];
 	$hash = md5_file($tmp);
@@ -37,7 +41,6 @@ if (!empty($_FILES['files'])) {
 	$size = $_FILES['files']['size'][0];
 	$filename = uniqid() . '_' . $origfilename;
 	$target = $uploadDir . $filename;	
-	$duplicate = false;
 	//Check DB for Hash.
 	
 	$pdo = new PDO($dsn, $user, $pass, $options);
@@ -45,28 +48,22 @@ if (!empty($_FILES['files'])) {
 	$stmt->execute(['hash' => $hash]);
 	$exists = $stmt->fetchColumn();
 	if ($exists) {
-    		die("This file has already been uploaded.");
+    		$message = 'This file has already been uploaded.';
 	}
 	
 	if ($debug) {
 		echo "<pre>";
 		var_dump('$tmp: ', $tmp);
 		var_dump('$_FILES: ', $_FILES);
-		var_dump('$duplicate: ', $duplicate);
 		var_dump('$hash: ', $hash);
 		var_dump('$filename: ', $filename);
 		var_dump('$target: ', $target);
-		var_dump('$dupeFilename: ', $dupeFilename);
 		echo "</pre>";
         }
 
-	if ($duplicate) {
-		$fileUrl = 'uploads/' . basename($dupeFilename);
-    		echo "Duplicate detected: $name already uploaded as <a href='$fileUrl'>$dupeFilename</a><br>";
-		exit;
-	}	
-	if (move_uploaded_file($tmp, $target)) {
-		echo "Uploaded: <a href='uploads/$filename'>$filename</a><br>";
+	if (move_uploaded_file($tmp, $target) && !$exists) {
+		$message = "Upload successful!";
+		$url = 'uploads/' . rawurlencode($filename);
 		//Insert DB
 		$stmt = $pdo->prepare("
 				INSERT INTO files (hash, stored_name, original_name, size)
@@ -77,12 +74,44 @@ if (!empty($_FILES['files'])) {
 			'stored_name' => $filename,
 			'original_name' => $origfilename,
 			'size' => $size,
-		]);
-		
+		]);	
 	} else {
-		echo "Failed to move uploaded file: $filename<br>";
-		var_dump($tmp, $target, is_uploaded_file($tmp), file_exists($target));
-	};
+		if ($exists) { 
+		$message = "This file has already been uploaded.";
+		} else {
+		$message = "Failed to move uploaded file: $filename<br>";
+		}
+		//var_dump($tmp, $target, is_uploaded_file($tmp), file_exists($target));
+	}
 	} else {
-	echo "No file uploaded.";
+		echo "No file uploaded.";
+	}
 }
+?>
+<!DOCTYPE html>
+<html>
+	<link rel="stylesheet" href="css/style.css">
+	<head>
+		<title>filehost</title>
+	</head>
+	<body>
+		<h1>filehost</h1>
+			<p>The lightweight & secure file hosting web service</p>
+			<form enctype="multipart/form-data" method="POST">
+				<input type="file" id="upload-input" name="files[]" multiple>
+				<input type="submit" value="Submit">
+			</form>
+			<?php if ($message): ?>
+    			<p><?= htmlspecialchars($message) ?></p>
+			<a href="<?= htmlspecialchars($url) ?>" target="_blank"><?= htmlspecialchars($url) ?></a>
+			<?php endif; ?>
+			
+		<footer>
+			<p><sub>
+			<a href="www.github.com/twilldigg/filehost">Source Code</a>
+			 Project loosely based on my experience of the project 
+			<a href="www.github.com/nokonoko/Uguu">nokonoko/Uguu<a>
+			</sub></p>
+		</footer>
+	</body>
+</html>
